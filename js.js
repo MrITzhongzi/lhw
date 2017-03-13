834c6ea16c4c360c993a95bbbfb070b1a82d7978
lhw <lhw@qq.com>
2017-03-13 23:30:22 +0800

    个人页面

---
+++ js.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/3/13.
+ */
+var btn = document.querySelector('button');
+btn.onclick = function(){
+    var name = document.querySelector('.name')
+    name.style.display = 'block';
+}
