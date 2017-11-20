7f3a086f32ae0ee6a737c9b04f21a615cd8eccf8
lhw <lhw@qq.com>
2017-11-20 20:13:18 +0800

    第一页完成

--- script/index.js
+++ script/index.js
@@ -1,6 +1,20 @@
 /**
  * Created by Administrator on 2017/3/13.
  */
-// $(function(){
-
-// })
+window.onload = function(){
+    var jiantouTimer = null;
+    var jiantouEle = document.querySelectorAll('.swiper-slide')
+    
+   
+    // 设置底部下箭头的闪烁
+    jiantouTimer = setInterval(function(){
+        jiantouEle.forEach(function(e){
+            if(e.classList.contains('jiantou-animate')){
+                e.classList.remove('jiantou-animate')
+            }else {
+                e.classList.add('jiantou-animate')
+            }
+            
+        })
+    },600)
+}
